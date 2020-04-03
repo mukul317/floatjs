83af0f449a2399741afa50814126f5c4d41468a7
Mukul Sharma <mukul.sharma@naukri.com>
2020-04-03 15:42:13 +0530

    Revert "Merge branch 'suggester' of https://github.com/mukul317/floatjs"
    This reverts commit 6d2832ce61bdde7440a78ec731841a817703a7cc, reversing
    changes made to eed9983b5ab14b41543817d70b7bebd022b25398.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { TDroopeConfig } from "./Droope/interface";
-import { TSugConfig } from "./SuggesterStable/interface";
 import droopeData from "./Droope/data";
-import suggesterData from "./SuggesterStable/data";
 import Droope from "./Droope";
-import Suggester from "./SuggesterStable";
+import "./Suggester";
 
 ((): void => {
     const inputElement: HTMLInputElement | null = document.querySelector(".select-box-input");
@@ -24,43 +22,5 @@ import Suggester from "./SuggesterStable";
         };
         const DroopeInstance = Droope(options, droopeData);
         console.log(DroopeInstance);
-    }
-})();
-
-((): void => {
-    const inputElement: HTMLInputElement | null = document.querySelector(".suggester-select-box-input");
-    const listingElement: HTMLElement | null = document.querySelector(".suggester-select-box-listing");
-    const displayElement: HTMLElement | null = inputElement;
-    if (inputElement && listingElement && displayElement) {
-        const options: TSugConfig = {
-            domId: "demoSuggester",
-            inputElement,
-            listingElement,
-            displayElement,
-            selectLimit: 6,
-            displayListOnFocus: true,
-            displayDecorationList: [],
-            startSearchAfter: 2,
-            // displayBehaviour: "tag",
-            noResultErrorMessage: "Sorry no result",
-            checkboxes: false,
-            sanitiseString: false,
-            specialCharactersAllowedList: [";"],
-            // eslint-disable-next-line @typescript-eslint/camelcase
-            relatedConcept_dataLayer: true,
-            source: "server",
-            category: "top",
-            maxSuggestions: 15,
-            edge: 0,
-            placeholder: false,
-            trackUserInteraction: false, // to track user Interaction
-            grouping: true,
-            isPrefetch: true,
-            relatedConceptsLimit: 5,
-            debounceTimeout: 500,
-            defaultPrefetchLookup: true
-        };
-        const SuggesterInstance = Suggester(options, suggesterData);
-        console.log("Suggester", SuggesterInstance);
     }
 })();
